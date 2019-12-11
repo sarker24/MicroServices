9cecff4b12be3357a243bafddd98aa23a38ccf91
Murad <sarker24@gmail.com>
2019-12-11 17:58:10 +0100

    Add Model

---
+++ MicroServiceProject.BookingFlight.Domain/Model/FeedBack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServiceProject.BookingFlight.Domain.Model
+{
+  public  class FeedBack
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
